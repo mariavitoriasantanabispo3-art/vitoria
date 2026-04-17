a80728e5e229789ad2fd9ab09e38796244402b52
mariavitoriasantanabispo3-art <mariavitoriasantanabispo3@gmail.com>
2026-04-17 15:29:52 -0300

    finalizando o codigo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function mudar_conteudo_elemento() {
+
+  document.getElementById("h1").innerHTML = "Hello JavaScript!";
+}
+
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
